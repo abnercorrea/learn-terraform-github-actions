d6c25057ea4d2a6900cba7991e19d3d0e57d0899
Abner Correa <abnercorreajr@gmail.com>
2024-05-11 07:17:42 +0100

    Adding free tier ami and instance type

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-north-1"
 }
 
 resource "random_pet" "sg" {}
@@ -25,21 +25,27 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    name   = "image-id"
+    # Ubuntu Server 22.04 LTS - HVM (Free tier)
+    values = ["ami-011e54f70c1c91e17"]
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+#  filter {
+#    name   = "name"
+#    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-22.04-amd64-server-*"]
+#  }
+#
+#  filter {
+#    name   = "virtualization-type"
+#    values = ["hvm"]
+#  }
+#
+#  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"  # Free tier
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
